562718d0d8c5738c1cd9ea2c83223596bf1e6336
Manrich121 <manrich121@gmail.com>
2026-03-12 15:58:18 +0200

    add app icon with script to resize

--- flutter_app/macos/Runner/AppDelegate.swift
+++ flutter_app/macos/Runner/AppDelegate.swift
@@ -3,11 +3,29 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
-
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-    return true
-  }
+    
+    var statusBar: NSStatusItem?
+    var popover = NSPopover()
+    var flutterViewController: FlutterViewController?
+    
+    override func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+        statusBar = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        if let button = statusBar?.button {
+            button.image = NSImage(named: "MenuBarIcon")
+            button.action = #selector(togglePopover(_:))
+        }
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject) {
+        print("clicked!")
+        
+        if let button = statusBar?.button {
+                if popover.isShown {
+                    popover.performClose(sender)
+                } else {
+                    print("Open Popover")
+                }
+            }
+    }
 }
